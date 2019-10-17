0b70261eee5551c32abb108e2f424b22b38d8832
tom frazer <tomfrazer@toms-MacBook-Air.local>
2019-10-17 14:40:18 +0800

    Where I got up to

--- load_dmr_data.R
+++ load_dmr_data.R
@@ -90,4 +90,95 @@ pData(ips_dmr_mCG)$CellType <- celltype
 pData(ips_dmr_mCG)$Replicate <- substr(sampleNames, 
                               nchar(sampleNames), nchar(sampleNames))
 
+
+#PRACTICE
+data("BS.chr21")
+print(celltype)
+?dmrseq
+
+# Added column to data (unsure why this was needed?)
+pData(BS.chr21)$CellType <- celltype
+pData(BS.chr21)$Replicate <- substr('Rep', 
+                              nchar(1), nchar(2))
+
+pData(BS.chr21)
+
+# Set testcovariate to be cell type
+testCovariate <- 'CellType'
+
+# Followed code exactly to filter no coverage values and only select cell types of interest
+loci.idx <- which(DelayedMatrixStats::rowSums2(getCoverage(BS.chr21, type="Cov")==0) == 0)
+sample.idx <- which(pData(BS.chr21)$CellType %in% c("imr90", "h1"))
+
+bs.filtered <- BS.chr21[loci.idx, sample.idx]
+
+# Now moving on to attempting identification of DMRs
+testCovariate <- "CellType"
+regions <- dmrseq(bs=BS.chr21[240001:260000,],
+                  cutoff = 0.05,
+                  testCovariate=testCovariate)
+
+show(regions)
+library("BiocParallel")
+
+
+?dmrseq
+
+# ?dmrseq says that when blocksize is TRUE (minimum consideration = 5000bp)
+# all other parameters form the DMR analysis on large scale methyaltion blocks is specified
+
+#input this code to run comparison of methylation on larger blocks
+testCovariate <- "CellType"
+blocks <- dmrseq(bs=BS.chr21[120001:125000,],
+                 cutoff = 0.05,
+                 testCovariate=testCovariate,
+                 block = TRUE,
+                 minInSpan = 500,
+                 bpSpan = 5e4,
+                 maxGapSmooth = 1e6,
+                 maxGap = 5e3)
+
+head(blocks)
+
+# Number of significant regions 
+sum(regions$qval < 0.05)
+
+# Proportion of significant regions hypermethylated
+sigRegions <- regions[regions$qval < 0.05,]
+sum(sigRegions$stat > 0) / length(sigRegions)
+
+# A bit tricky with the plotting
+?plotDMRs
+
+annoTrack <- getAnnot("hg18")
+yes
+
+plotDMRs(BS.chr21, regions=regions[1,], testCovariate="CellType",
+         annoTrack="hg18")
+
+
+plotDMRs(BS.chr21, regions=blocks[1,], testCovariate="CellType",
+         annoTrack="hg18")
+
+# extraction of mean methylation differences
+rawDiff <- meanDiff(BS.chr21, dmrs=sigRegions, testCovariate="CellType")
+str(rawDiff)
+# This was the end of practice stuff
+
+
+
+
+
+
+
+# RETRY ON MY DATA
+ips_dmr_mCG <- read.csv(file = "ips_dmr_mCG.csv", row.names = 1)
+
+head(ips_dmr_mCG)
+
+# My issue is here, I want to get 'ips_dmr_mCG' into a format that can be opened by pData to resemble the practice data
+# How should I go about putting this data into a format that can be read this way?
 pData(ips_dmr_mCG)
+
+
+
